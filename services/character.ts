c847d93a356e227b166c64acd46919c8aa71b88f
tavodevcode <tavodevcode@gmail.com>
2024-02-11 22:36:46 -0600

    📄 Add cache control to character API request

--- services/character.ts
+++ services/character.ts
@@ -4,7 +4,7 @@ import { API_URL } from '@/lib/constants'
 export const getCharacters = async (): Promise<CharacterResult> => {
   await new Promise((resolve) => setTimeout(resolve, 2000))
 
-  const response = await fetch(`${API_URL}/character`)
+  const response = await fetch(`${API_URL}/character`, { cache: 'no-store' })
   const episodies = await response.json()
 
   return episodies
