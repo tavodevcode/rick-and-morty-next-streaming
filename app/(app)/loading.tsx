9b3ebe6914f1efeb32507dd087ccd71242f7aadd
tavodevcode <tavodevcode@gmail.com>
2024-02-11 22:22:33 -0600

    📄 Add Loading component

---
+++ app/(app)/loading.tsx
@@ -0,0 +1,4 @@
+export default function Loading (): JSX.Element {
+  // You can add any UI inside Loading, including a Skeleton.
+  return <div className="w-screen h-screen flex justify-center items-center bg-red-200" >loading</div>
+}
